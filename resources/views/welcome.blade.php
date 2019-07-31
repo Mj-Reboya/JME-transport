f57fa32d6da985ed55a4417d286ae38adb609d39
Jonel Pante <jonel@xtendly.com>
2019-07-31 12:39:35 +0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>JmetForms</title>
+        <base href="/">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="favicon.ico">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="styles.css"> -->
+        <link href="assets/semantic.min.css" rel="stylesheet" />
+    </head>
+    <body>
+    <app-root></app-root>
+    <!-- <script src="js/jmet-forms.js"></script>
+    <script src="js/styles-es5.js"></script> -->
+    <script src="js/runtime-es2015.js" type="module"></script>
+    <script src="js/polyfills-es2015.js" type="module"></script>
+    <script src="js/runtime-es5.js" nomodule></script>
+    <script src="js/polyfills-es5.js" nomodule></script>
+    <script src="js/styles-es2015.js" type="module"></script>
+    <script src="js/styles-es5.js" nomodule></script>
+    <script src="js/vendor-es2015.js" type="module"></script>
+    <script src="js/main-es2015.js" type="module"></script>
+    <script src="js/vendor-es5.js" nomodule></script>
+    <script src="js/main-es5.js" nomodule></script>
+    </body>
+</html>
