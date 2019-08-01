a9187ac5003c86c2e7799b7783ca0652d401f94b
Jonel Pante <jonel@xtendly.com>
2019-08-01 10:05:39 +0800

    updates build

---
+++ resources/views/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>JmetForms</title>
+    <base href="/" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/x-icon" href="favicon.ico" />
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet"
+    />
+    <link href="assets/semantic.min.css" rel="stylesheet" />
+  <link rel="stylesheet" href="styles.48bbb76b35c04ec30ae2.css"></head>
+  <body>
+    <app-root></app-root>
+  <script src="runtime-es2015.703a23e48ad83c851e49.js" type="module"></script><script src="polyfills-es2015.505fe7908b00d09f6b03.js" type="module"></script><script src="runtime-es5.465c2333d355155ec5f3.js" nomodule></script><script src="polyfills-es5.4eb1dbdf86ed1e4f9b6d.js" nomodule></script><script src="main-es2015.4ae0b726c81a6dc92eb5.js" type="module"></script><script src="main-es5.9a1ba37f2b4f1441bff1.js" nomodule></script></body>
+</html>
