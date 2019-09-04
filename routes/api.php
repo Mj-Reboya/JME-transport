42d38518dba48b52e56364decb794ff85f02d92c
Jonel Pante <jonel@xtendly.com>
2019-09-04 17:15:27 +0800

    test error

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use PHPJasper\PHPJasper as PHPJasper;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -62,7 +64,15 @@ Route::get('/generate-pdf/{pdf_name}', function ($pdf_name, Request $request) {
     $options
   )->output();
 
-  exec($out);
+  exec('/var/www/html/jme-terminal-app/vendor/geekcom/phpjasper/bin/jasperstarter/bin/jasperstarter process "/var/www/html/jme-terminal-app/routes/../app/Reports/proof-of-delivery.jasper" -o "/tmp/pdf/jme_5d6f75212c86c1000" -f pdf -P  transaction_id="1000"   -t mysql -u webdev -p "W7u2mW^d8&;jZB2," -H 127.0.0.1 -n jme --db-port 3306', $_output, $retval);
+  var_dump($_output);
+  var_dump($retval);
+  // $process = new Process('sh /folder_name/file_name.sh');
+  // $process->run();
+
+  // if (!$process->isSuccessful()) {
+  //   throw new ProcessFailedException($process);
+  // }
 
   // $jasper->process(
   //   $input,
