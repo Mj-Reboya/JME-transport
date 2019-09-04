d37d2b9ebd05d269a9fdcac8ede6de5a704591b1
Jonel Pante <jonel@xtendly.com>
2019-09-04 17:25:25 +0800

    tried to use symphony process

--- routes/api.php
+++ routes/api.php
@@ -64,15 +64,15 @@ Route::get('/generate-pdf/{pdf_name}', function ($pdf_name, Request $request) {
     $options
   )->output();
 
-  exec('/var/www/html/jme-terminal-app/vendor/geekcom/phpjasper/bin/jasperstarter process "/var/www/html/jme-terminal-app/routes/../app/Reports/proof-of-delivery.jasper" -o "/tmp/pdf/jme_5d6f75212c86c1000" -f pdf -P  transaction_id="1000"   -t mysql -u webdev -p "W7u2mW^d8&;jZB2," -H 127.0.0.1 -n jme --db-port 3306', $_output, $retval);
-  var_dump($_output);
-  var_dump($retval);
-  // $process = new Process('sh /folder_name/file_name.sh');
-  // $process->run();
+  // exec('', $_output, $retval);
+  // var_dump($_output);
+  // var_dump($retval);
+  $process = new Process('/var/www/html/jme-terminal-app/vendor/geekcom/phpjasper/bin/jasperstarter process "/var/www/html/jme-terminal-app/routes/../app/Reports/proof-of-delivery.jasper" -o "/tmp/pdf/jme_5d6f75212c86c1000" -f pdf -P  transaction_id="1000" -t mysql -u webdev -p "W7u2mW^d8&;jZB2," -H 127.0.0.1 -n jme --db-port 3306');
+  $process->run();
 
-  // if (!$process->isSuccessful()) {
-  //   throw new ProcessFailedException($process);
-  // }
+  if (!$process->isSuccessful()) {
+    throw new ProcessFailedException($process);
+  }
 
   // $jasper->process(
   //   $input,
