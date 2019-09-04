e06808a31b4c6ab70d348feb54b82bb942d5754d
Jonel Pante <jonel@xtendly.com>
2019-09-04 15:16:33 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -52,24 +52,17 @@ Route::get('/generate-pdf/{pdf_name}', function ($pdf_name, Request $request) {
   $input =  __DIR__ . '/../app/Reports/proof-of-delivery.jasper';
   $output = __DIR__ . '/../app/Reports/temp/' . uniqid('jme_') . $transaction_id;
 
-  try {
-    $options['db_connection'] = $db_conf;
-    $options['params'] = $params;
-    $jasper = new PHPJasper;
-    $jasper->process(
-      $input,
-      $output,
-      $options
-    )->execute();
-    return response()->download($output . '.pdf', 'Proof-of-delivery' . uniqid('_jme_') . $transaction_id . '.pdf', [
-      'code' => 400
-    ])->deleteFileAfterSend(true);
-  } catch (\Throwable $th) {
-    var_dump($th);
-    return response()->json([
-      'message' => 'server error'
-    ], 500);
-  }
+  $options['db_connection'] = $db_conf;
+  $options['params'] = $params;
+  $jasper = new PHPJasper;
+  $jasper->process(
+    $input,
+    $output,
+    $options
+  )->execute();
+  return response()->download($output . '.pdf', 'Proof-of-delivery' . uniqid('_jme_') . $transaction_id . '.pdf', [
+    'code' => 400
+  ])->deleteFileAfterSend(true);
 });
 
 
